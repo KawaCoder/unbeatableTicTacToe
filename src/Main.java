bb4f9ea1fdcbd4000ca2d438577e621a3bf1c837
DR34M-M4K3R <67145585+DR34M-M4K3R@users.noreply.github.com>
2021-09-18 13:06:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * License holder: DR34M-M4K3R#7751*/
+
 
 
 import java.util.*;
